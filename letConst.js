7f13a4c455b4f5c6a1b9c2d15203d259d2b8cbe7
Hasib787 <mr.hasib45@gmail.com>
2021-02-01 22:17:46 +0600

    added a new file

---
+++ letConst.js
@@ -0,0 +1,16 @@
+const Name = "Hasib";
+console.log(Name);
+
+const numbers = [5, 8, 6];
+numbers.push(12);
+console.log(numbers);
+
+let patientName = "Rakin";
+patientName = 'Rakesh';
+console.log(patientName);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+}
+console.log(i);
