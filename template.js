b4e7e14c77886ee03edcff8a6538191ad5c5441b
Hasib787 <mr.hasib45@gmail.com>
2021-02-01 23:11:12 +0600

    added a new file called template

---
+++ template.js
@@ -0,0 +1,10 @@
+const firstName='Justin';
+const lastName='Teror';
+const fullName= `${firstName} ${lastName} is a very good boy.`;
+console.log(fullName);
+
+
+const multiLine=`Hi,
+I'm Bob.
+I need some help.`;
+console.log(multiLine);
