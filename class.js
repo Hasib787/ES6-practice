ef5f32ca1901f35ec10f1d8aba49ec186de21003
Hasib787 <mr.hasib45@gmail.com>
2021-02-02 13:03:36 +0600

    added a new file name ingeritacnce.js

--- class.js
+++ class.js
@@ -1,11 +1,11 @@
-class student {
+class Student {
     constructor(sId, sName) {
         this.id = sId;
         this.name = sName;
         this.schoolName = 'Barishal Model School & Collage';
     }
 }
-const student1 = new student(124, "Hasib");
-const student2 = new student(56, "Noman");
-const student3 = new student(12, 'Rakin');
+const student1 = new Student(124, "Hasib");
+const student2 = new Student(56, "Noman");
+const student3 = new Student(12, 'Rakin');
 console.log(student1, student2, student3);
